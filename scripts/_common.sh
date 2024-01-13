87cdc14411dce013876b4132ae30e7de1a17d0cf
Félix Piédallu <felix@piedallu.me>
2024-01-13 12:13:28 +0100

    Packaging v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,11 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION="7.3"
-
-# dependencies used by the app
-pkg_dependencies="php${YNH_PHP_VERSION}-mysql"
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
